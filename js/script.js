6ca2aafdb083ae76ae42f9bce3f155e9420d6326
jaga <jagadheesh6@gmail.com>
2020-09-11 20:14:57 +0530

    Re-structured Everything

---
+++ js/script.js
@@ -0,0 +1,23 @@
+
+
+
+window.onload = ()=>{
+    var a = document.getElementById('tri')
+
+    a.click()
+}
+
+
+var b1 = document.getElementById('b1')
+
+b1.addEventListener('click',()=>{
+
+    window.location.href = "connect-1.0.html";
+})
+
+var b2 = document.getElementById('b2')
+
+b2.addEventListener('click', () => {
+
+    window.location.href = "connect-2.0.html";
+})
